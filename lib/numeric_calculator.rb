f8a9a209e6c0380decedb38175e8c9c54f7f6a05
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-12 10:08:06 -0600

    User is given the difference of two numbers

--- lib/numeric_calculator.rb
+++ lib/numeric_calculator.rb
@@ -2,4 +2,7 @@ class NumericCalculator
   def add(num1, num2)
     num1 + num2
   end
+  def subtract(num1, num2)
+    num1 - num2
+  end
 end
--- spec/numeric_calculator_spec.rb
+++ spec/numeric_calculator_spec.rb
@@ -10,6 +10,15 @@ describe NumericCalculator do
   actual_value = sum_of_two_numbers.add(1,2)
 
   expect(actual_value).to eq expected_value
-
  end
+
+  it "Returns the difference of two numbers" do
+
+    diff_of_two_numbers = NumericCalculator.new
+
+    expected_value = 3
+    actual_value = diff_of_two_numbers.subtract(5,2)
+
+    expect(actual_value).to eq expected_value
+  end
 end
