d9904eaaac9108560ffa6be68201f038dbb012a7
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-12 10:26:27 -0600

    Changed the names of some moethods for better readibility

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,16 @@
 class StringCalculator
- def add(str)
-    sum = 0
-    @array_of_numbers = str.split(',')
-    @array_of_numbers.each do |num|
-    sum += num.to_i
-  end
+ def add(string)
+    if string.empty?
+      0
+    else
+      sum = 0
+      numbers = string.split(',')
+      numbers.each do |num|
+      sum += num.to_i
+    end
   sum
   end
+ end
 end
 
 
