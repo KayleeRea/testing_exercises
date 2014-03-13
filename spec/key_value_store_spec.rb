2b0dfd7e7db02c7f1616c4b7ac8e2cefbe00b8ab
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-13 06:47:53 -0600

    Refactored the test and modified the test name to what is actually happening in the test, i.e. get method

--- spec/key_value_store_spec.rb
+++ spec/key_value_store_spec.rb
@@ -3,15 +3,12 @@ require "key_value_store"
 
 describe KeyValueStore do
 
-  it "Allows the user to add a value stored under a key." do
+  it "Allows the user to get a value stored under a key." do
     keyvalue = KeyValueStore.new
 
     keyvalue.add("Flash", "Dog")
 
-    expected_value = "Dog"
-    actual_value = keyvalue.get("Flash")
-
-    expect(actual_value).to eq expected_value
+    expect(actual_value = keyvalue.get("Flash")).to eq expected_value = "Dog"
 
   end
 
