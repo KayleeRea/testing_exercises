40f6c6f60764d216b0504ad4a29a4faf45dda4d5
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-16 23:13:34 -0600

    Created a lib, spec and path file for BufferedLogger
    defined my class and started the framework for the tests.

---
+++ lib/buffered_logger.rb
@@ -0,0 +1,6 @@
+class BufferedLogger
+  def initialize(logger)
+    @logger = logger
+    "logger.rb"
+  end
+end
---
+++ spec/buffered_logger_spec.rb
@@ -0,0 +1,8 @@
+require "buffered_logger"
+
+describe BufferedLogger do
+  it "goes to the right path file" do
+
+  end
+end
+
