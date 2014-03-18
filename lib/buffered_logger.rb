8daebe63ae783ed77f6841fcb4b8fdbf11bb70cb
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-17 21:53:25 -0600

    Created a lib, spec and a path file. Also created a test that checks to see if the code writes a message after 5 times of the log method is called upon and prints the message as well as the full time

--- lib/buffered_logger.rb
+++ lib/buffered_logger.rb
@@ -1,6 +1,28 @@
-class BufferedLogger
-  def initialize(logger)
-    @logger = logger
-    "logger.rb"
+
+class Logger
+
+  def initialize(path)
+    @path = path
+    @count = 0
+    @array = []
+  end
+
+  def log(message)
+
+    line = "#{message} #{Time.now}"
+
+
+    @array.push(line)
+
+
+    if @array.length > 6
+      nil
+    else
+      File.open(@path, "w") do |x|
+        @array.each do |line|
+          x.puts line
+        end
+      end
+    end
   end
 end
--- spec/buffered_logger_spec.rb
+++ spec/buffered_logger_spec.rb
@@ -1,8 +1,19 @@
 require "buffered_logger"
 
-describe BufferedLogger do
-  it "goes to the right path file" do
 
+describe Logger do
+
+  it "writes the message only after the log method has been called 5 times" do
+  logger = Logger.new(File.expand_path("../../lib/new_file.rb", __FILE__))
+
+  5.times do
+    logger.log("hello")
+  end
+
+  actual = "hello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\n"
+  expected = File.open(File.expand_path("../../lib/new_file.rb", __FILE__)).read
+
+  expect(actual).to eq (expected)
   end
 end
 
